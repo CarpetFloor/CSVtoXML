097b91628da448a6d5de1a2c9437fd8be761d641
CarpetFloor <dylan.bartness@gmail.com>
2024-07-31 22:43:05 -0500

    Read file

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 let input = document.querySelector("input");
 input.addEventListener("change", readFile);
 
+let partsUnformatted = [];
+
 function readFile() {
-    console.log("testing");
+    let fileReader = new FileReader();
+    fileReader.readAsText(input.files[0]);
+
+    fileReader.onload = function() {
+        let result = fileReader.result;
+        let split = result.split("\n");
+        // don't need header
+        split.shift();
+
+        for(let part of split) {
+            partsUnformatted.push(part);
+        }
+    }
+
 }
+
+let output = "<inventory>"
+
+/**
+ * -Need everything in INVENTORY tag
+ * -Every part must be in ITEM tag
+ * -Need ITEMTYPE, ITEMID, COLOR, and QTY
+ */
+
+output += "<inventory>";
