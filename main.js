0bb193310e8da10c267bddef1e6aef4eba85b260
CarpetFloor <dylan.bartness@gmail.com>
2024-07-31 23:29:25 -0500

    Config CSV

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+// config stuff
+let configInputElems = [...(document.querySelector(".input").children)];
+for(let elem of configInputElems) {
+    let id = elem.id;
+
+    elem.addEventListener("click", function() {
+        addConfigOption(id);
+    })
+}
+
+let config = [];
+let configDisplay = document.querySelector(".input").nextElementSibling;
+
+function addConfigOption(id) {
+    if(id == "delete") {
+        let childCount = [...configDisplay.children].length;
+        
+        if(childCount > 1) {
+            let deleteElem = configDisplay.children[childCount - 1];
+            
+            config.splice(config.indexOf(deleteElem.innerText), 1);
+            deleteElem.remove();
+            
+            childCount = [...configDisplay.children].length;
+            let commaElem = configDisplay.children[childCount - 1];
+            commaElem.remove();
+        }
+        else if(childCount == 1) {
+            let deleteElem = configDisplay.children[childCount - 1];
+            
+            config.splice(config.indexOf(deleteElem.innerText), 1);
+            deleteElem.remove();
+        }
+    }
+    else if(!(config.includes(id)) || (id == "na")) {
+        config.push(id);
+
+        if([...configDisplay.children].length > 0) {
+            let comma = document.createElement("p");
+            comma.className = "comma";
+            comma.innerText = ",";
+            
+            configDisplay.appendChild(comma);
+        }
+    
+        let elem = document.createElement("p");
+        elem.className = "nonComma";
+        elem.innerText = id;
+    
+        configDisplay.appendChild(elem);
+    }
+}
+
 let input = document.querySelector("input");
 input.addEventListener("change", readFile);
 
@@ -17,7 +70,6 @@ function readFile() {
             partsUnformatted.push(part);
         }
     }
-
 }
 
 let output = "<inventory>"
